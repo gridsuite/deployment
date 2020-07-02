6fc13b8c945042b77fb238dcc81327ef7e4693ec
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2020-07-02 10:28:39 +0200

    Update image version for cronJob containers
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- .github/scripts/image-tag-to-digest.py
+++ .github/scripts/image-tag-to-digest.py
@@ -41,6 +41,19 @@ def get_digest(repository, tag, token):
     else:
         response.raise_for_status()
 
+def update_images(containers):
+    for container in containers:
+        image = container['image']
+
+        # parse image and get tag
+        (registry, repository, tag) = parse_image(image)
+
+        # get digest from tag (only works with docker.io)
+        if registry == 'docker.io':
+            token = get_token(repository)
+            digest = get_digest(repository, tag, token)
+            # replace tag by digest
+            container['image'] = repository + '@' + digest
 
 documents = list(yaml.load_all(sys.stdin.read(), Loader=yaml.FullLoader))
 for document in documents:
@@ -48,17 +61,9 @@ for document in documents:
         spec = document['spec']
         if 'template' in spec:
             containers = spec['template']['spec']['containers']
-            for container in containers:
-                image = container['image']
-
-                # parse image and get tag
-                (registry, repository, tag) = parse_image(image)
-
-                # get digest from tag (only works with docker.io)
-                if registry == 'docker.io':
-                    token = get_token(repository)
-                    digest = get_digest(repository, tag, token)
-                    # replace tag by digest
-                    container['image'] = repository + '@' + digest
+            update_images(containers)
+        if 'jobTemplate' in spec:
+            containers = spec['jobTemplate']['spec']['template']['spec']['containers']
+            update_images(containers)
 
 print(yaml.dump_all(documents))
