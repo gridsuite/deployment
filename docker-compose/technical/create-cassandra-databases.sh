4f8a839e37fbab59412e4d04a716ac8ddc4e7bc6
Seddik Yengui <seddik.yengui@rte-france.com>
2022-04-07 16:17:08 +0200

    Switching to Postgres and Liquibase of case import job

--- docker-compose/technical/create-cassandra-databases.sh
+++ docker-compose/technical/create-cassandra-databases.sh
@@ -4,13 +4,10 @@ set -e
 
 cat > create_keyspaces.cql <<EOF
 CREATE KEYSPACE IF NOT EXISTS "${DATABASE_PREFIX_NAME}cgmes_assembling" WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-CREATE KEYSPACE IF NOT EXISTS "${DATABASE_PREFIX_NAME}import_history" WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1 };
 EOF
 
 echo "USE ${DATABASE_PREFIX_NAME}cgmes_assembling;" >> init_keyspaces.cql
 curl https://raw.githubusercontent.com/gridsuite/cgmes-assembling-job/main/src/main/resources/cgmes_assembling.cql >> init_keyspaces.cql
-echo "USE ${DATABASE_PREFIX_NAME}import_history;" >> init_keyspaces.cql
-curl https://raw.githubusercontent.com/gridsuite/case-import-job/main/src/main/resources/import_history.cql >> init_keyspaces.cql
 
 until cqlsh -f /create_keyspaces.cql; do
   echo "cqlsh: Cassandra is unavailable to create keyspaces - will retry later"
