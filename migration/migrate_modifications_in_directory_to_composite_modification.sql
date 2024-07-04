068214bed45cb077d663b76199ef085ace8fa452
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-07-04 13:12:17 +0200

    create new directory for composite modif

--- migration/migrate_modifications_in_directory_to_composite_modification.sql
+++ migration/migrate_modifications_in_directory_to_composite_modification.sql
@@ -7,8 +7,11 @@ with
 insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
 select n.id as id, m.id as modification_id, row_number() over () from modifications_ids m full join new_composite n on true=true;
 delete from directory.element where type = 'MODIFICATION';
+new_directory as (insert into directory.element(id, name, owner, parent_id, type, description, creation_date, last_modification_date, last_modified_by, stash_date, stashed)
+values (gen_random_uuid(), 'Modification_composite_migration', 'admin',
+        null, 'DIRECTORY', null, '2024-07-02 14:24:50.296498 +00:00', '2024-07-02 14:24:50.296498 +00:00', 'admin', null, false) return id);
 insert into directory.element(id, name, owner, parent_id, type, description, creation_date, last_modification_date, last_modified_by, stash_date, stashed)
-values ((select id from networkmodifications.composite_modification), 'Generated-modification-composite', 'admin',
-        '054e7336-c9cc-4dd9-9817-7a8b38d2a667', 'MODIFICATION', null, '2024-07-02 14:24:50.296498 +00:00',
+values (new_composite, 'Generated_modification_composite', 'admin',
+        new_directory, 'MODIFICATION', null, '2024-07-02 14:24:50.296498 +00:00',
         '2024-07-01 21:14:15.517000 +00:00', 'admin', null, false);
 commit;
