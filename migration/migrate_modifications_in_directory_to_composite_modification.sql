797f04e26b4da2f468c520e87566eea19ccc10c6
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-07-05 16:49:12 +0200

    fix script

--- migration/migrate_modifications_in_directory_to_composite_modification.sql
+++ migration/migrate_modifications_in_directory_to_composite_modification.sql
@@ -1,31 +1,13 @@
 begin transaction isolation level serializable;
+select *, gen_random_uuid() as new_id into MODIFICATION_ELEMENTS from directory.element where type = 'MODIFICATION';
 
-with
+update directory.element e set id = (select modifs.new_id from MODIFICATION_ELEMENTS modifs where modifs.id=e.id) where type = 'MODIFICATION';
 
-    dir_element_with_m_id as (select *, gen_random_uuid() as new_id from directory.element where type = 'MODIFICATION'),
+insert into networkmodifications.modification (id, date, group_id, modifications_order, stashed, message_type, message_values, type)
+select new_id, NULL, NULL, NULL, false, NULL, NULL, 'COMPOSITE_MODIFICATION' from MODIFICATION_ELEMENTS;
 
-    new_modification as (
+insert into networkmodifications.composite_modification (id) (select new_id from MODIFICATION_ELEMENTS);
 
-        insert into networkmodifications.modification(id, date, group_id, modifications_order, stashed, message_type, message_values, type)
-
-            select m.new_id, NULL, NULL, NULL, false, NULL, NULL, 'COMPOSITE_MODIFICATION' from dir_element_with_m_id m
-
-            returning id
-    ),
-
-    new_composite as (insert into networkmodifications.composite_modification(id) select id from new_modification returning id),
-
-    new_element_modification as (
-
-        insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
-
-            select m.new_id, m.id, row_number() over () as modifications_order from dir_element_with_m_id m
-
-            left join new_composite nc on m.new_id=nc.id
-
-            returning id, modification_id
-    )
-
-update directory.element e set id = (select n.id from new_element_modification n where n.modification_id=e.id) where type = 'MODIFICATION';
-
+insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
+    (select new_id, id, 0 from MODIFICATION_ELEMENTS);
 commit;
