1339d46b5707c99336c2f897204458adaf572bb7
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-07-05 08:34:48 +0200

    wrap each modification by composite modification

--- migration/migrate_modifications_in_directory_to_composite_modification.sql
+++ migration/migrate_modifications_in_directory_to_composite_modification.sql
@@ -1,17 +1,31 @@
-begin isolation level repeatable read;
+begin transaction isolation level serializable;
+
 with
-    new_modification as (insert into networkmodifications.modification(id, date, group_id, modifications_order, stashed, message_type, message_values, type)
-        values (gen_random_uuid(), NULL, NULL, NULL, false, NULL, NULL, 'COMPOSITE_MODIFICATION') returning id),
-    new_composite as (insert into networkmodifications.composite_modification(id) (select id from new_modification) returning id),
-    modifications_ids as (select id from directory.element where type = 'MODIFICATION')
-insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
-select n.id as id, m.id as modification_id, row_number() over () from modifications_ids m full join new_composite n on true=true;
-delete from directory.element where type = 'MODIFICATION';
-new_directory as (insert into directory.element(id, name, owner, parent_id, type, description, creation_date, last_modification_date, last_modified_by, stash_date, stashed)
-values (gen_random_uuid(), 'Modification_composite_migration', 'admin',
-        null, 'DIRECTORY', null, '2024-07-02 14:24:50.296498 +00:00', '2024-07-02 14:24:50.296498 +00:00', 'admin', null, false) return id);
-insert into directory.element(id, name, owner, parent_id, type, description, creation_date, last_modification_date, last_modified_by, stash_date, stashed)
-values (new_composite, 'Generated_modification_composite', 'admin',
-        new_directory, 'MODIFICATION', null, '2024-07-02 14:24:50.296498 +00:00',
-        '2024-07-01 21:14:15.517000 +00:00', 'admin', null, false);
+
+    dir_element_with_m_id as (select *, gen_random_uuid() as new_id from directory.element where type = 'MODIFICATION'),
+
+    new_modification as (
+
+        insert into networkmodifications.modification(id, date, group_id, modifications_order, stashed, message_type, message_values, type)
+
+            select m.new_id, NULL, NULL, NULL, false, NULL, NULL, 'COMPOSITE_MODIFICATION' from dir_element_with_m_id m
+
+            returning id
+    ),
+
+    new_composite as (insert into networkmodifications.composite_modification(id) select id from new_modification returning id),
+
+    new_element_modification as (
+
+        insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
+
+            select m.new_id, m.id, row_number() over () as modifications_order from dir_element_with_m_id m
+
+            left join new_composite nc on m.new_id=nc.id
+
+            returning id, modification_id
+    )
+
+update directory.element e set id = (select n.id from new_element_modification n where n.modification_id=e.id) where type = 'MODIFICATION';
+
 commit;
