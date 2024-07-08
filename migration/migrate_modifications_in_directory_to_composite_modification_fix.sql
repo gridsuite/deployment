353f88f6da9ff55eb95f363a4cc1679d4c5185a5
Ghazoua Rehili <ghazwarhili@gmail.com>
2024-07-08 14:54:17 +0200

    script to wrap unitary modifications by composite modification (#419)
    Signed-off-by: REHILI Ghazwa <ghazwarhili@gmail.com>

---
+++ migration/migrate_modifications_in_directory_to_composite_modification_fix.sql
@@ -0,0 +1,14 @@
+begin transaction isolation level serializable;
+update networkmodifications.modification e
+set date = (select creation_date from modification_elements where new_id = e.id),
+    modifications_order = 0,
+    message_type = 'COMPOSITE_MODIFICATION',
+    message_values = '{}'
+where type = 'COMPOSITE_MODIFICATION'
+  AND date IS NULL
+  AND modifications_order IS NULL
+  AND message_type IS NULL
+  AND message_values IS NULL;
+
+drop table modification_elements;
+commit;
