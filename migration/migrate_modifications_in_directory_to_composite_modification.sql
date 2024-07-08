2326bc620baf924ba03b95c8647851952f795245
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-07-08 14:01:16 +0200

    review remarqs

--- migration/migrate_modifications_in_directory_to_composite_modification.sql
+++ migration/migrate_modifications_in_directory_to_composite_modification.sql
@@ -4,10 +4,11 @@ select *, gen_random_uuid() as new_id into MODIFICATION_ELEMENTS from directory.
 update directory.element e set id = (select modifs.new_id from MODIFICATION_ELEMENTS modifs where modifs.id=e.id) where type = 'MODIFICATION';
 
 insert into networkmodifications.modification (id, date, group_id, modifications_order, stashed, message_type, message_values, type)
-select new_id, current_date, NULL, 0, false, 'COMPOSITE_MODIFICATION', '{}', 'COMPOSITE_MODIFICATION' from MODIFICATION_ELEMENTS;
+select new_id, creation_date, NULL, 0, false, 'COMPOSITE_MODIFICATION', '{}', 'COMPOSITE_MODIFICATION' from MODIFICATION_ELEMENTS;
 
 insert into networkmodifications.composite_modification (id) (select new_id from MODIFICATION_ELEMENTS);
 
 insert into networkmodifications.composite_modification_sub_modifications(id, modification_id, modifications_order)
     (select new_id, id, 0 from MODIFICATION_ELEMENTS);
+drop table MODIFICATION_ELEMENTS;
 commit;
--- migration/migrate_modifications_in_directory_to_composite_modification_fix.sql
+++ migration/migrate_modifications_in_directory_to_composite_modification_fix.sql
@@ -1,6 +1,6 @@
 begin transaction isolation level serializable;
-update networkmodifications.public.modification e
-set date = CURRENT_DATE,
+update networkmodifications.modification e
+set date = (select creation_date from modification_elements where id = e.id),
     modifications_order = 0,
     message_type = 'COMPOSITE_MODIFICATION',
     message_values = '{}'
@@ -9,4 +9,6 @@ where type = 'COMPOSITE_MODIFICATION'
   AND modifications_order IS NULL
   AND message_type IS NULL
   AND message_values IS NULL;
+
+drop table modification_elements;
 commit;
