319a28fc97ef16ec9da5e18c7124f457e0a161a5
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-07-04 11:41:31 +0200

    delete table in concerned microservice with liquibase
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- migration/delete_script_contingencyList_elements_from_directory.sql
+++ migration/delete_script_contingencyList_elements_from_directory.sql
@@ -1,6 +1,3 @@
--- Step 1: Delete script filter elements from directory database
+-- Delete script filter elements from directory database
 DELETE FROM directory.element
 WHERE id IN (SELECT id FROM actions.script_contingency_list);
-
--- Step 2: Drop the actions.script_contingency_list table
-DROP TABLE actions.script_contingency_list;
--- migration/delete_script_filters_elements_from_directory.sql
+++ migration/delete_script_filters_elements_from_directory.sql
@@ -1,6 +1,3 @@
--- Step 1: Delete script filter elements from directory database
+-- Delete script filter elements from directory database
 DELETE FROM directory.element
 WHERE id IN (SELECT id FROM filters.script_filter);
-
--- Step 2: Drop the filters.script_filter table
-DROP TABLE filters.script_filter;
