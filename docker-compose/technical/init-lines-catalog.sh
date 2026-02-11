51d070d0359bfe98d954982394f7d5a850c847da
Joris Mancini <joris.mancini_externe@rte-france.com>
2026-02-11 12:36:29 +0100

    refactor(docker-compose): use include and simplify usage
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- docker-compose/technical/init-lines-catalog.sh
+++ docker-compose/technical/init-lines-catalog.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 function init_lines_catalog()
 {
   LINES_CATALOG=/init-data/lines-catalog.json.gz
@@ -10,11 +8,8 @@ function init_lines_catalog()
   fi
 }
 
-if [ "$PROJECT_DIR_NAME" == "$PROJECT_STUDY_DIR_NAME" ] || [ "$PROJECT_DIR_NAME" == "$PROJECT_SUITE_DIR_NAME" ]
-then
-  until init_lines_catalog
+until init_lines_catalog
   do
     echo "curl: network-modification-server is unavailable to initialize data - will retry later"
     sleep 5
   done
-fi
